508808e30b80881c106c887f6648124c93d8c69c
DmitryMurashov <dmtr.murashov@gmail.com>
2022-09-26 14:09:49 +0300

    [fft:dsps_fft2r] Impl | step-overload for f32-based fft

--- modules/fft/include/dsps_fft2r.hpp
+++ modules/fft/include/dsps_fft2r.hpp
@@ -119,4 +119,42 @@ esp_err_t dsps_fft2r_sc16_ansi_step(T in_data, int N, int16_t* sc_table)
 	return result;
 }
 
+template <class T>
+esp_err_t dsps_fft2r_fc32_ansi_step(T *data, int N, float *w)
+{
+    if (!dsp_is_power_of_two(N)) {
+        return ESP_ERR_DSP_INVALID_LENGTH;
+    }
+    if (!dsps_fft2r_initialized) {
+        return ESP_ERR_DSP_UNINITIALIZED;
+    }
+
+    esp_err_t result = ESP_OK;
+
+    int ie, ia, m;
+    float re_temp, im_temp;
+    float c, s;
+    ie = 1;
+    for (int N2 = N / 2; N2 > 0; N2 >>= 1) {
+        ia = 0;
+        for (int j = 0; j < ie; j++) {
+            c = w[2 * j];
+            s = w[2 * j + 1];
+            for (int i = 0; i < N2; i++) {
+                m = ia + N2;
+                re_temp = c * data[2 * m] + s * data[2 * m + 1];
+                im_temp = c * data[2 * m + 1] - s * data[2 * m];
+                data[2 * m] = data[2 * ia] - re_temp;
+                data[2 * m + 1] = data[2 * ia + 1] - im_temp;
+                data[2 * ia] = data[2 * ia] + re_temp;
+                data[2 * ia + 1] = data[2 * ia + 1] + im_temp;
+                ia++;
+            }
+            ia += N2;
+        }
+        ie <<= 1;
+    }
+    return result;
+}
+
 #endif // _dsps_fft2r_HPP_
